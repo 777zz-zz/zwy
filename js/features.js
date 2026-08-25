b3d2928efdceefb8db30346e64a65c66d834fb69
777zz-zz <2548134748@qq.com>
2026-08-25 15:52:56 +0800

    Update features.js

--- js/features.js
+++ js/features.js
@@ -186,7 +186,7 @@
 
 (function() {
     var KEY = 'keepaliveAudioEnabled';
-    var SRC = 'https://imgbed.heliar.top/i/CxzYwk59Cohc1f_O_copy_013C85AC-A1BC-4020-9F71-F2B585F8D374.m4a';
+    var SRC = 'https://imgbed.heliar.top/i/O0Mwce5nFtKHsotC_Silence .m4a';
     var _audio = null;
     var _unlockBound = false;
 
